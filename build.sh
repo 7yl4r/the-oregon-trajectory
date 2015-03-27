c9f99705a236a3641b6e7c5d959cff4b4b806a1a
Brian Erikson <briankerikson@gmail.com>
2015-03-27 20:35:54 +0100

    Fixed build script's folder references

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 echo 'compiling less css...'
-./node_modules/.bin/lessc -x ./app.less > ./bundle.css
+./ng_modules/.bin/lessc -x ./app.less > ./bundle.css
 
 #echo 'transpiling .coffee into .js...'
-#./node_modules/.bin/coffee --output js/coffee --compile coffee
+#./ng_modules/.bin/coffee --output js/coffee --compile coffee
 
 echo 'browserify-ing module dependencies...'
-./node_modules/.bin/browserify -t coffeeify app.js -o bundle.js
+./ng_modules/.bin/browserify -t coffeeify app.js -o bundle.js
 
 echo 'starting localhost server for testing...'
 python -m SimpleHTTPServer
