7f981c88a1be18307f290d8b6f257e75b27c3166
Tylar Murray <murray.tylar@gmail.com>
2015-03-30 13:07:48 -0400

    build, build, build. must remember to build.

--- bundle.js
+++ bundle.js
@@ -397,8 +397,34 @@ app.directive("shop", function() {
 });
 
 app.controller("ShopController", ['$scope', 'data', function($scope, data){
-    this.tab = 1;
     this.data = data;
+    this.item_consumables = [
+        {
+            name: 'Rocket Fuel',
+            description: "You won't get very far without this.",
+            price: 2,
+            image: "/assets/Flat-UI-master/img/icons/png/Infinity-Loop.png",
+            key: "fuel"
+        },{
+            name: 'Rations',
+            description: "Not just freeze-dried ice cream.",
+            price: 1,
+            image: "/assets/Flat-UI-master/img/icons/png/Infinity-Loop.png",
+            key: "rations"
+        }
+    ];
+    this.tab = 1;
+    this.activeItem = this.item_consumables[0];
+
+    this.buy = function(item){
+        // for consumables:
+        if (typeof item.key !== 'undefined') {
+            this.data[item.key] += 1
+        } else {
+            // TODO: apply item some other way
+        }
+        this.data.money -= item.price;
+    };
 
     this.isSet = function(checkTab) {
         return this.tab === checkTab;
@@ -408,82 +434,19 @@ app.controller("ShopController", ['$scope', 'data', function($scope, data){
         this.tab = activeTab;
     };
 
-    this.buy = function(item){
-        // for consumables:
-        if (typeof item.key !== 'undefined') {
-            this.data[item.key] += 1
-        } else {
-            // TODO: apply item some other way
-            ;
+    this.setActiveItem = function(itemName) {
+        for (var i = 0; i < this.item_consumables.length; i++) {
+            var item = this.item_consumables[i];
+            if (itemName == item.name) this.activeItem = item;
         }
-        this.data.money -= item.price;
+    };
+
+    this.ifItemActive = function(itemName) {
+        if (itemName == this.activeItem.name) {
+            return "item-container-selected";
+        }
+        return '';
     }
-
-    this.item_consumables = [
-        {
-            name: 'Rocket Fuel',
-            description: "You won't get very far without this.",
-            price: 1,
-            image: "",
-            key: "fuel"
-        },{
-            name: 'Rations',
-            description: "Not just freeze-dried ice cream.",
-            price: 1,
-            image: "",
-            key: "rations"
-        }
-        /*
-        {
-            name: 'Life Support',
-            description: "Placeholder text",
-            price: 2,
-            image: ""},
-        {
-            name: 'Food',
-            description: "Placeholder text",
-            price: 3,
-            image: ""},
-        {
-            name: 'Water',
-            description: "Placeholder text",
-            price: 4,
-            image: ""},
-        {
-            name: 'Drill Bit',
-            description: "Placeholder text",
-            price: 5,
-            image: ""},
-        {
-            name: 'Spare Engine',
-            description: "Placeholder text",
-            price: 6,
-            image: ""}
-            */
-    ];
-
-    this.item_mods = [
-        {
-            name: 'Rocket Payload 1',
-            description: "Placeholder text",
-            price: 1,
-            image: ""},
-        {
-            name: 'Rocket Payload 2',
-            description: "Placeholder text",
-            price: 1,
-            image: ""},
-        {
-            name: 'Rocket 1',
-            description: "Placeholder text",
-            price: 1,
-            image: ""},
-        {
-            name: 'Rocket 2',
-            description: "Placeholder text",
-            price: 1,
-            image: ""}
-    ];
 }]);
 
 module.exports = angular.module('shop').name;
