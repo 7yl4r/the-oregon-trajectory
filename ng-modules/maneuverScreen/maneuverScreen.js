bcd497f81235264566e53c4e63179c4e37ec0ae4
Ashlynn <ashlynn.wonderland@gmail.com>
2015-03-31 20:42:24 -0400

    Fixed a naming issue and started work on module

--- ng-modules/maneuverScreen/maneuverScreen.js
+++ ng-modules/maneuverScreen/maneuverScreen.js
@@ -3,10 +3,27 @@ require('angular');
 var app = angular.module('maneuver-screen', []);
 
 app.directive("maneuver-screen", function() {
-    return {
-        restrict: 'E',
-        templateUrl: "/ng-modules/maneuver-screen/maneuver-screen.html"
-    };
+  return {
+    restrict: 'E',
+    templateUrl: "/ng-modules/maneuver-screen/maneuverScreen.html"
+  };
 });
+
+app.controller('ManeuverScreenCtrl', function($scope){
+  $scope.choices = [
+    {
+      'text': 'Take the high path',
+      'response': function(){console.log('Chose "Take the high path"')}
+    },
+    {
+      'text': 'Take the low path',
+      'response': function(){console.log('Chose "Take the low path"')}
+    }
+  ];
+
+  //background image
+
+  //title
+}
 
 module.exports = angular.module('maneuver-screen').name;
