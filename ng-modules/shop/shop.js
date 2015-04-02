860de8bf729098425ce8da36d5f70b395a396556
Brian Erikson <briankerikson@gmail.com>
2015-04-02 12:17:44 +0200

    Added a travel info div to the shop

--- ng-modules/shop/shop.js
+++ ng-modules/shop/shop.js
@@ -66,7 +66,16 @@ app.controller("ShopController", ['$scope', 'data', function($scope, data){
             return "item-container-selected";
         }
         return '';
-    }
+    };
+
+    this.getDistToNext = function() {
+        for (var loc in data.locations) {
+            var pos = data.locations[loc];
+            if (pos > data.distanceTraveled)
+                return pos - data.distanceTraveled;
+        }
+        return 0;
+    };
 }]);
 
 module.exports = angular.module('shop').name;
