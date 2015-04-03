11f561dc3a43d6ef6a209450600c2bb2e875736c
Tylar Murray <murray.tylar@gmail.com>
2015-04-03 15:58:28 -0400

    added sound to you-win

--- ng-modules/youWin/youWin.js
+++ ng-modules/youWin/youWin.js
@@ -1,4 +1,6 @@
 require('angular');
+Nodule = require('nodule');
+Howl = require('howler');
 
 var app = angular.module('you-win', []);
 
@@ -8,5 +10,26 @@ app.directive("youWin", function() {
         templateUrl: "/ng-modules/youWin/youWin.html"
     };
 });
+
+app.controller("youWinCtrl", [ '$rootScope', '$scope', function($rootScope, $scope){
+    vm = this;
+    vm.music =  new Howl({
+        urls: ['assets/sound/music/winning/winning.ogg', 'assets/sound/music/winning/winning.mp3'],
+        loop: true
+    });
+    clickSound = new Howl({
+        urls: ['assets/sound/effects/select/select.ogg', 'assets/sound/effects/select/select.mp3']
+    })
+
+    vm.onEnter = function(){
+        $scope.$emit('changeMusicTo', vm.music);
+    }
+    vm.nodule = Nodule($rootScope, 'you-win', vm.onEnter);
+
+    vm.mainMenu = function(){
+        clickSound.play();
+        $scope.$emit('switchToModule', 'main-menu');
+    }
+}]);
 
 module.exports = angular.module('you-win').name;
