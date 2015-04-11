ff6ae5b8be962d88c3ec40e80debfea7a575af38
Geno Roupsky <geno@roupsky.name>
2015-04-11 14:23:28 +0300

    [situation] display simple step

--- ng-modules/situation/situation.js
+++ ng-modules/situation/situation.js
@@ -2,7 +2,8 @@ require('angular');
 Howl = require('howler');    // for sounds (if you need them)
 Nodule = require('nodule');  // for nodule helpers
 
-var app = angular.module('situation', []);
+var app = angular.module('situation', [
+]);
 
 app.directive("situation", function() {
     return {
@@ -11,9 +12,27 @@ app.directive("situation", function() {
     };
 });
 
-app.controller("situationController", ['data', '$scope', '$rootScope', function(data, $scope, $rootScope) {
+app.controller("situationController", ['data', '$scope', '$rootScope', '$sce', function(data, $scope, $rootScope, $sce) {
+
     var vm = this;
-    vm.nodule = new Nodule($rootScope, 'situation');
+    vm.nodule = new Nodule($rootScope, 'situation', function() {
+        console.log("onEntry", arguments);
+        if (!arguments) {
+          vm.dialogTree = {
+
+          }
+        }
+    });
+
+    $scope.step = {
+      story: $sce.trustAsHtml("<h1>You're here</h1>"),
+      question: "What do you do know, luke?",
+      options: [
+        {name: "fight"},
+        {name: "run"},
+        {name: "surrender"},
+      ],
+    }
 
     // your controller code here
     vm.clickedTheButton = function(){
