9314eff78ee671d282e913c68f84750ff29c99fd
Sara <thefocusedwriter@gmail.com>
2015-04-12 11:37:56 -0500

    Update dialogFinish.js

--- ng-modules/asteroidMining/dialogFinish.js
+++ ng-modules/asteroidMining/dialogFinish.js
@@ -1,19 +1,19 @@
 module.exports = function (stats, callback) {
   return {
     state_exit: {
-      story: "You broke out of orbit. You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel.",
+      story: "You got too far from the asteroid and broke orbit. You finish with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel. Better luck next time!",
       choices: [
         {name: "Continue", next:callback}
       ]
     },
     state_end: {
-      story: "The asteroid escaped. You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel.",
+      story: "The asteroid got away. You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel. Better luck next time!",
       choices: [
         {name: "Continue", next:callback}
       ]
     },
     state_crash: {
-      story: "You crashed with the asteroid. You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel.",
+      story: "You got too close to the asteroid and damaged your ship. You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel. Better luck next time!",      
       choices: [
         {name: "Continue", next:callback}
       ]
