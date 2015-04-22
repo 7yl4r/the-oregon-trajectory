2879fa76600e75477ad0dbbed5850989b224e4d9
Tylar Murray <murray.tylar@gmail.com>
2015-04-22 09:48:41 -0400

    asteroid being mined now shrink as they are mined

--- ng-modules/asteroidMining/dialogFinish.js
+++ ng-modules/asteroidMining/dialogFinish.js
@@ -1,22 +1,27 @@
+
+
 module.exports = function (stats, callback) {
-  return {
-    state_exit: {
-      story: "You got too far from the asteroid and broke orbit. You finish with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel. Better luck next time!",
-      choices: [
+    var conclusion = "You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel. Better luck next time!";
+    var continueChoice = [
         {name: "Continue", next:callback}
-      ]
+    ]
+
+    return {
+    state_exit: {
+      story: "You got too far from the asteroid and broke orbit. " + conclusion,
+      choices: continueChoice
     },
     state_end: {
-      story: "The asteroid got away. You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel. Better luck next time!",
-      choices: [
-        {name: "Continue", next:callback}
-      ]
+      story: "The asteroid got away. " + conclusion,
+      choices: continueChoice
     },
     state_crash: {
-      story: "You got too close to the asteroid and damaged your ship. You are finishing with <strong>"+stats.credits+"</strong> credits and <strong>"+stats.fuel+"</strong> fuel. Better luck next time!",      
-      choices: [
-        {name: "Continue", next:callback}
-      ]
+      story: "You got too close to the asteroid and damaged your ship. " + conclusion,
+      choices: continueChoice
+    },
+    state_depleted: {
+      story: "This asteroid has been depleted of resources. " + conclusion,
+       choices: continueChoice
     }
   }
 }
