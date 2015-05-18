bf17abef7dd7c4b91fd908ef0a71ab67ca30c587
Tylar Murray <murray.tylar@gmail.com>
2015-05-18 14:41:31 -0400

    small improvments to end screen. closes #104

--- ng-modules/gameOver/gameOver.js
+++ ng-modules/gameOver/gameOver.js
@@ -5,33 +5,32 @@ Howl = require('howler');
 var app = angular.module('game-over', []);
 
 app.directive("gameOver", function() {
-  return {
-    restrict: 'E',
-    templateUrl: "ng-modules/gameOver/gameOver.html"
-  };
+    return {
+        restrict: 'E',
+        templateUrl: "ng-modules/gameOver/gameOver.html"
+    };
 });
 
-
 app.controller("gameOverCtrl", [ '$rootScope', '$scope', 'data', function($rootScope, $scope, data){
-  vm = this;
-  vm.data = data;
-  vm.music =  new Howl({
-    urls: ['assets/sound/music/Losing.ogg', 'assets/sound/music/Losing.mp3'],
-    loop: true
-  });
-  clickSound = new Howl({
-    urls: ['assets/sound/effects/select/select.ogg', 'assets/sound/effects/select/select.mp3']
-  })
+    vm = this;
+    vm.data = data;
+    vm.music =  new Howl({
+        urls: ['assets/sound/music/Losing.ogg', 'assets/sound/music/Losing.mp3'],
+        loop: true
+    });
+    clickSound = new Howl({
+        urls: ['assets/sound/effects/select/select.ogg', 'assets/sound/effects/select/select.mp3']
+    })
 
-  vm.onEnter = function(){
-    $scope.$emit('changeMusicTo', vm.music);
-  }
-  vm.nodule = Nodule($rootScope, 'game-over', vm.onEnter);
+    vm.onEnter = function(){
+        $scope.$emit('changeMusicTo', vm.music);
+    }
+    vm.nodule = Nodule($rootScope, 'game-over', vm.onEnter);
 
-  vm.mainMenu = function(){
-    clickSound.play();
-    $scope.$emit('switchToModule', 'main-menu');
-  }
+    vm.mainMenu = function(){
+        clickSound.play();
+        $scope.$emit('switchToModule', 'main-menu');
+    }
 }]);
 
 module.exports = angular.module('game-over').name;
