f12192eab6fbda0dfddc2fb171e27a29e783fa6b
7yl4r <murray.tylar@gmail.com>
2016-11-23 23:11:34 -0500

    setup preload

--- index.coffee
+++ index.coffee
@@ -1,4 +1,4 @@
-require('howler')
+require('phaser')
 
 # show warning before navigating from the page
 window.onbeforeunload = () ->
@@ -7,16 +7,30 @@ window.onbeforeunload = () ->
 window.fillVersionSpans = () ->
     $('.version-number').text(globalData.version)
 
-window.globalData = {};
-globalData.gameDir = '';
+window.globalData = {
+    gameDir:'',
+    baseUrl:(if location.host == '7yl4r.github.io' then '/the-oregon-trajectory/' else '/')
+};
 
 $(document).ready( ()->
+
+    # version number display
     version = '0.0.0';
     $.get(globalData.gameDir+'/package.json', {}, (data, textStatus, jqXHR)->
         globalData.version = data.version
         window.fillVersionSpans()
     , 'json');
 
-)
+    globalData.game = new Phaser.Game(800, 600, Phaser.CANVAS,
+        'game-container-div',
+        {
+            preload: require('phaser-preload'),
+            create: require('phaser-create'),
+            update: require('phaser-update'),
+            render: require('phaser-render')
+        }
+    );
 
-module.exports = globalData
+    # game volume mute
+    # globalData.game.sound.mute = false;
+)
---
+++ phaserLifecycle/preload.js
@@ -0,0 +1,34 @@
+module.exports = function() {  // preload function
+  globalData.game.load.image('a-small', globalData.baseUrl+'assets/sprites/asteroids/p0-small.png');
+  globalData.game.load.image('space', globalData.baseUrl+'assets/backgrounds/milky_way_bg.png');
+  globalData.game.load.image('bullet', 'http://examples.phaser.io/assets/games/asteroids/bullets.png');
+
+  // NOTE: these audio snippets are duplicates of those already loaded in game.sounds (and we could use those instead)
+  globalData.game.load.audio('shot1',
+      [
+          'assets/sound/effects/shot1/shot1.mp3',
+          'assets/sound/effects/shot1/shot1.ogg',
+          'assets/sound/effects/shot1/shot1.wav'
+      ]
+  );
+  globalData.game.load.audio('shot2',
+      [
+          'assets/sound/effects/shot2/shot2.mp3',
+          'assets/sound/effects/shot2/shot2.ogg'
+      ]
+  );
+  globalData.game.load.audio('clunk',
+    [
+        'assets/sound/effects/clunk/clunk.mp3',
+        'assets/sound/effects/clunk/clunk.ogg',
+        'assets/sound/effects/clunk/clunk.wav'
+    ]
+  );
+  globalData.game.load.audio('propel',
+      [
+          'assets/sound/effects/propellant/propellant.mp3',
+          'assets/sound/effects/propellant/propellant.ogg',
+          'assets/sound/effects/propellant/propellant.wav'
+      ]
+  );
+}
